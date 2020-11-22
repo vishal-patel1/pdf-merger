15d56feaa8e07d28a3efaf1062afad962953e1c4
vishal-patel1 <67033860+vishal-patel1@users.noreply.github.com>
2020-11-22 14:12:53 -0600

    Graphical Application Limited Capabilities
    This GUI apps now kind of works

--- graphical.py
+++ graphical.py
@@ -5,38 +5,79 @@ try:
     from tkinter import filedialog
     from PyPDF3 import PdfFileReader, PdfFileMerger
 except ImportError:
-    import Tkinter as tk
-    import ttk
-    import tkFileDialog as filedialog
+    import tkinter as tk
+    import tkinter.ttk as ttk
+    from tkinter import filedialog
  
 root = tk.Tk()
-root.geometry('400x400')
+root.geometry('600x400')
 
 style = ttk.Style(root)
 style.theme_use("clam")
- 
-def openFilesAndConvert():
+
+#global variables
+fileName = ""
+pdfs = []
+outputDirectory = ""
+
+def openFiles():
+    global pdfs
     pdfs = filedialog.askopenfilenames(
     	parent=root,
     	initialdir='/',
     	filetypes=[
     		("pdf", "*.pdf")])
 
+def getFileName():
+    global fileName
+    global e
+
+    fileName = e.get()
+
+    if(".pdf" in fileName):
+        fileName = fileName
+    elif(".pdf" not in fileName):
+        fileName += ".pdf"
+
+    print(fileName)
+
+def openOutputDirectory():
+    global outputDirectory
+    outputDirectory = filedialog.askdirectory(
+        parent = root,
+        initialdir='/'
+    )
+
+    print("file will be saved at " + outputDirectory)
+
+
+def convert():
+    global outputDirectory
+    os.chdir(outputDirectory)
     output = PdfFileMerger()
+
     for pdf in pdfs:
         input_pdf = PdfFileReader(pdf)
         output.append(input_pdf)
 
     print("writing the combined pdf")
-    output.write("idkifthiswillwork.pdf")
+    output.write(fileName)
     print("finished")
 
 
-ttk.Button(root, text="Open files", command=openFilesAndConvert).grid(row=1, column=0, padx=4, pady=4, sticky='ew')
-e = ttk.Entry(root, width=50)
-e.pack()
+fileOpener = ttk.Button(root, text="Open PDFs", command=openFiles)
+fileOpener.grid(row=1, column=0, padx=4, pady=4, sticky='ew')
 
-name = tk.StringVar()
+e = ttk.Entry(root)
+e.grid(row=2, column=0, padx=4, pady=4, sticky='ew')
 
+confirmFileName = ttk.Button(root, text="Confirm File Name", command=getFileName)
+confirmFileName.grid(row=2, column=1, padx=4, pady=4, sticky='ew')
+
+fileSaver = ttk.Button(root, text="Save File At", command = openOutputDirectory)
+fileSaver.grid(row=4, column=0)
+
+convertButton = ttk.Button(root, text="Convert!", command=convert)
+convertButton.grid(row=5, column=0, padx=4, pady=4, sticky='ew')
 
 root.mainloop()
