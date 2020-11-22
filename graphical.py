bfcdc80b05d965a3d96a1dc890d379bf931e350d
vishal-patel1 <67033860+vishal-patel1@users.noreply.github.com>
2020-11-22 14:45:34 -0600

    Graphical Component now works

--- graphical.py
+++ graphical.py
@@ -11,6 +11,7 @@ except ImportError:
  
 root = tk.Tk()
 root.geometry('600x400')
+root.title("Merge PDFs")
 
 style = ttk.Style(root)
 style.theme_use("clam")
@@ -38,18 +39,21 @@ def getFileName():
         fileName = fileName
     elif(".pdf" not in fileName):
         fileName += ".pdf"
+    else:
+        fileName = "merge.pdf"
 
-    print(fileName)
+    output = "file will be saved as " + fileName
+    textBox.config(text=output)
 
 def openOutputDirectory():
-    global outputDirectory
+    global outputDirectory, textBox
     outputDirectory = filedialog.askdirectory(
         parent = root,
         initialdir='/'
     )
 
-    print("file will be saved at " + outputDirectory)
-
+    output = "file will be saved at " + outputDirectory
+    textBox.config(text=output)
 
 def convert():
     global outputDirectory
@@ -60,24 +64,26 @@ def convert():
         input_pdf = PdfFileReader(pdf)
         output.append(input_pdf)
 
-    print("writing the combined pdf")
+    textBox.config(text="writing the combined pdf")
     output.write(fileName)
-    print("finished")
-
+    textBox.config(text="finished")
 
 fileOpener = ttk.Button(root, text="Open PDFs", command=openFiles)
 fileOpener.grid(row=1, column=0, padx=4, pady=4, sticky='ew')
 
 e = ttk.Entry(root)
-e.grid(row=2, column=0, padx=4, pady=4, sticky='ew')
+e.grid(row=1, column=1, padx=4, pady=4, sticky='ew')
 
 confirmFileName = ttk.Button(root, text="Confirm File Name", command=getFileName)
 confirmFileName.grid(row=2, column=1, padx=4, pady=4, sticky='ew')
 
 fileSaver = ttk.Button(root, text="Save File At", command = openOutputDirectory)
-fileSaver.grid(row=4, column=0)
+fileSaver.grid(row=1, column=2)
 
-convertButton = ttk.Button(root, text="Convert!", command=convert)
-convertButton.grid(row=5, column=0, padx=4, pady=4, sticky='ew')
+convertButton = ttk.Button(root, text="Merge!", command=convert)
+convertButton.grid(row=1, column=3, padx=4, pady=4, sticky='ew')
+
+textBox = tk.Label(root, fg="red")
+textBox.grid(row = 5, column=1, padx=4, pady=4, sticky='ew')
 
 root.mainloop()
