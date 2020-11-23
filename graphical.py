baaa2a430646f45aa0fbab5346fb2f50394cb6a0
vishal-patel1 <67033860+vishal-patel1@users.noreply.github.com>
2020-11-23 17:53:08 -0600

    Working on the packaging

--- graphical.py
+++ graphical.py
@@ -1,10 +1,9 @@
-#global variables
+# global variables
 fileName = ""
 pdfs = []
 outputDirectory = ""
-theme = ""
 
-#imports
+# imports
 try:
     from ttkthemes import ThemedTk
     from tkinter import ttk
@@ -12,50 +11,48 @@ try:
     from PyPDF3 import PdfFileReader, PdfFileMerger
     from tkinter import filedialog
     import os
-    theme = "themed"
 except ImportError:
+    from ttkthemes import ThemedTk
     from tkinter import ttk
     import tkinter as tk
     from os import path
     from PyPDF3 import PdfFileReader, PdfFileMerger
     from tkinter import filedialog
     import os
-    theme = "not themed"
 
-if (theme == "themed"):
-    root = ThemedTk(theme="yaru")
-elif(theme == "not themed"):
-    root = tk.Tk()
+root = ThemedTk(theme="breeze")
 
 root.geometry('475x400')
 root.title("Merge PDFs")
-root.iconbitmap("icon.ico")
+root.iconbitmap("icon.icns")
 root.iconphoto(False, tk.PhotoImage(file="icon.png"))
 style = ttk.Style(root)
 ttk.Button(root, text="Quit", command=root.destroy).place(x=340, y=360)
 
-def openFiles():
+
+def open_files():
     global pdfs
     pdfs = filedialog.askopenfilenames(
-    	parent=root,
-    	initialdir='/',
-    	filetypes=[
-    		("pdf", "*.pdf")])
+        parent=root,
+        initialdir='/',
+        filetypes=[
+            ("pdf", "*.pdf")])
 
-    if(pdfs == ""):
+    if pdfs == "":
         output = "please select pdfs to merge"
         textBox.config(text=output, fg="red")
     else:
         output = "pdfs will be merged"
         textBox.config(text=output, fg="black")
 
-def saveAs():
+
+def save_as():
     global outputDirectory, fileName
 
     save = filedialog.asksaveasfilename(
-        parent = root,
+        parent=root,
         initialdir='/')
-    if(save == ""):
+    if save == "":
         output = "please pick a location and filename"
         textBox.config(text=output, fg="red")
     else:
@@ -63,25 +60,26 @@ def saveAs():
 
         outputDirectory = save[0:index]
 
-        fileName = save[index+1:len(save)]
+        fileName = save[index + 1:len(save)]
 
-        if(".pdf" in fileName):
+        if ".pdf" in fileName:
             fileName = fileName
-        elif(".pdf" not in fileName):
+        elif ".pdf" not in fileName:
             fileName += ".pdf"
         else:
             fileName = "merge.pdf"
-        
+
         output = "file will be saved at " + outputDirectory + " as " + fileName
         textBox.config(text=output, fg="black")
+
 
 def convert():
     global outputDirectory
 
-    if(outputDirectory==""):
+    if outputDirectory == "":
         output = "please select where to save the file and a name"
         textBox.config(text=output, fg="red")
-    elif(pdfs==""):
+    elif pdfs == "":
         output = "please select pdfs to merge"
         textBox.config(text=output, fg="red")
     else:
@@ -96,16 +94,17 @@ def convert():
         output.write(fileName)
         textBox.config(text="finished", fg="green")
 
-fileOpener = ttk.Button(root, text="Open PDFs", command=openFiles)
-fileOpener.place(x=20,y=20)
 
-saveAsButton = ttk.Button(root, text="Save as", command=saveAs)
-saveAsButton.place(x=175,y=20)
+fileOpener = ttk.Button(root, text="Open PDFs", command=open_files)
+fileOpener.place(x=20, y=20)
+
+saveAsButton = ttk.Button(root, text="Save as", command=save_as)
+saveAsButton.place(x=175, y=20)
 
 convertButton = ttk.Button(root, text="Merge!", command=convert)
-convertButton.place(x=330,y=20)
+convertButton.place(x=330, y=20)
 
-textBox = tk.Label(root, font=('bold'), wraplength=150, justify="center")
-textBox.place(x=135,y=100)
+textBox = tk.Label(root, font='bold', wraplength=150, justify="center")
+textBox.place(x=135, y=100)
 
 root.mainloop()
