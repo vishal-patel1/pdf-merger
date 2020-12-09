7de079eb4298dc55e37d54b91a168c1e5e390395
vishal-patel1 <67033860+vishal-patel1@users.noreply.github.com>
2020-12-09 10:53:15 -0600

    fixed some bugs

--- code/graphical.py
+++ code/graphical.py
@@ -2,6 +2,7 @@
 fileName = ""
 pdfs = []
 outputDirectory = ""
+osType  = ""
 
 # imports
 try:
@@ -10,22 +11,29 @@ try:
     from tkinter import ttk, filedialog
     import tkinter as tk
     from PyPDF3 import PdfFileReader, PdfFileMerger
-    import os
+    import os, platform
+    from PIL import ImageTk,Image 
 except ImportError:
     import importDependency
     from ttkthemes import ThemedTk 
     from tkinter import ttk, filedialog
+    from PIL import ImageTk,Image 
     import tkinter as tk
     from PyPDF3 import PdfFileReader, PdfFileMerger
-    import os
+    import os, platform
 
 root = ThemedTk(theme="plastik")
 
 root.geometry('475x400')
 root.title("Merge PDFs")
-#root.tk.call('wm', 'iconphoto', root._w, tk.PhotoImage(file="images/icon.png")
-root.iconbitmap("images/icon.icns")
-#root.iconphoto(False, tk.PhotoImage(file="images/icon.png"))
+
+# TODO add icon back depending on os
+if platform.system()== "Windows":
+    root.iconphoto(True, tk.PhotoImage(file="./images/icon.png"))
+elif platform.system() == "Darwin":
+    
+#elif platform.system() == "":
+
 
 
 def open_files():
@@ -38,10 +46,10 @@ def open_files():
             ("pdf", "*.pdf")])
     if pdfs == "":
         output = "please select pdfs to merge"
-        textBox.config(text=output, fg="red")
+        textBox.config(text=output, fg="red", justify="center")
     else:
         output = "pdfs will be merged"
-        textBox.config(text=output, fg="black")
+        textBox.config(text=output, fg="black", justify="center")
 
 
 def save_as():
@@ -52,7 +60,7 @@ def save_as():
         initialdir='/')
     if save == "":
         output = "please pick a location and filename"
-        textBox.config(text=output, fg="red")
+        textBox.config(text=output, fg="red", justify="center")
     else:
         index = save.rfind('/')
 
@@ -65,18 +73,17 @@ def save_as():
         else:
             fileName = "merge.pdf"
         output = "file will be saved at " + outputDirectory + " as " + fileName
-        textBox.config(text=output, fg="black")
-
+        textBox.config(text=output, fg="black", justify="center")
 
 def convert():
     global outputDirectory
 
     if outputDirectory == "":
         output = "please select where to save the file and a name"
-        textBox.config(text=output, fg="red")
+        textBox.config(text=output, fg="red", justify="center")
     elif pdfs == "":
         output = "please select pdfs to merge"
-        textBox.config(text=output, fg="red")
+        textBox.config(text=output, fg="red", justify="center")
     else:
         os.chdir(outputDirectory)
         output = PdfFileMerger()
@@ -85,14 +92,12 @@ def convert():
             input_pdf = PdfFileReader(pdf)
             output.append(input_pdf)
 
-        textBox.config(text="writing the combined pdf")
+        textBox.config(text="writing the combined pdf", justify="center")
         output.write(fileName)
-        textBox.config(text="finished", fg="green")
-
+        textBox.config(text="finished", fg="green", justify="center")
 
 def close():
     root.destroy()
-
 
 fileOpener = ttk.Button(root, text="Open PDFs", command=open_files)
 fileOpener.place(x=20, y=20)
@@ -100,13 +105,15 @@ fileOpener.place(x=20, y=20)
 saveAsButton = ttk.Button(root, text="Save as", command=save_as)
 saveAsButton.place(x=175, y=20)
 
-notice = ttk.Label(root, text="To ensure that the pdfs are merged in the proper order, please name the pdfs in alphabetical order.", wraplength=250, justify="center")
-notice.place(x=105, y=130)
+notice = ttk.Label(root, text="Notices: \n 1) To ensure that the pdfs are merged in the proper order, please name the pdfs in alphabetical order.  \n 2) Before merging pdfs directly from a scanner, open them up with a PDF editor and immediately save the document.", wraplength=250, justify="center")
+notice.place(x=105, y=230)
+
+
 
 convertButton = ttk.Button(root, text="Merge!", command=convert)
 convertButton.place(x=330, y=20)
 
-textBox = tk.Label(root, font='bold', wraplength=150, justify="center")
+textBox = ttk.Label(root, font='bold', wraplength=150, justify="center")
 textBox.place(x=135, y=100)
 
 ttk.Button(root, text="Quit", command=close).place(x=340, y=360)
