822bd8abeac7ece1ff8178a93b454921c7830715
vishal-patel1 <vishalpatel2003@hotmail.com>
2020-12-11 23:21:40 -0600

    removed legacy dependency code and docs

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
+
 setup(name="pdfMerger",
-    version="1.0",
-    description="A pdf merger",
-    long_description="An app that merges pdfs into one",
-    author="Vishal Patel",
-    packages=['code'],
-    install_requires=['subprocess', 'importDependency', 'ttkthemes', 'tkinter', 'PyPDF3', 'os'])
+      version="1.2",
+      description="A pdf merger",
+      long_description="An app that merges pdfs into one",
+      author="Vishal Patel",
+      packages=['code'],
+      install_requires=['subprocess', 'importDependency', 'tkinter', 'PyPDF3', 'os'])
