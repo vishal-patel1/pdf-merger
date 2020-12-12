bb5d38e92780002715bf5f1a9038dc8aa638bb9d
vishal-patel1 <vishalpatel2003@hotmail.com>
2020-12-11 23:30:48 -0600

    Revert "minor edits + removed extra dependencies + working on some image tests"
    This reverts commit 2d5c97be

--- code/importDependency.py
+++ code/importDependency.py
@@ -4,12 +4,13 @@ import subprocess
 # variables
 dependencies = {
     'PyPDF3': 'PyPDF3',
-    'setuptools': 'setuptools'
+    'setuptools': 'setuptools',
+    'ttkthemes': 'git+https://github.com/RedFantom/ttkthemes'
 }
 
 # install the apps
-for key, value in dependencies.items():
+for key,value in dependencies.items():
     try:
         __import__(key)
-    except ModuleNotFoundError:
+    except:
         subprocess.call('pip3 install ' + value, shell=True)
